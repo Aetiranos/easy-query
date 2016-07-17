fe9ff5e77c8642879e51157c688ddff012b5db9c
Aetiranos <aetiranos@crystaltome.com>
2016-07-17 09:06:55 -0500

    Updated breaking code. Removed all references to calc() which was not working properly in setting breakpoints and ranges. Updated inline documentation.

--- src/easy-query.js
+++ src/easy-query.js
@@ -5,23 +5,25 @@ var fs = require("fs");
  *
  * @param scss - Either a valid string representing the name of framework's breakpoints you'd wish to use or a custom array of
  *                 breakpoints. Must follow the structure shown below to be valid (given values are for example only).
- *                 The breakpoints will be named numerically so that one could have any number of breakpoints without running out of
- *                 size words (small, medium, etc). And so you wont be left with an ugly xxxxxxxxxl-breakpoint if you happened to go
- *                 that not-so-recommended route.
+ *                 The breakpoints will be named according to how many breakpoints are being used in the given framework/array.
+ *                 Please consult the README.md file to reference this table of breakpoint variable names.
  *
  *                 ['bp1-px','bp2-px','bp3-px']
  *
  *                 This will generate your breakpoint SCSS code that will resemble:
  *
- *                 $break-1: (null, calc(bp1 - 1px);
- *                 $break-2: (bp1, calc(bp2 - 1px);
- *                 $break-3: (bp2, calc(bp3 - 1px);
+ *                 (0.0625 === 1px @ 16px HTML)
+ *
+ *                 $break-1: (null, (bp1 - .0625);
+ *                 $break-2: (bp1, (bp2 - .0625);
+ *                 $break-3: (bp2, (bp3 - .0625);
  *                 $break-4: (bp3, null);
  *
  * @returns {string}
  */
 module.exports = function EasyQuery(scss) {
-    var framework;
+    var framework,
+        $rpx = .0625;
     var queued = "$screen: 'only screen' !default;\n\n@function lower-bound($range) {\n\n  @if length($range) <= 0 {\n    @return 0;\n  }\n\n  @return nth($range, 1);\n}\n\n@function upper-bound($range) {\n\n  @if length($range) < 2 {\n    @return 999999999999;\n  }\n\n  @return nth($range, 2);\n}\n\n";
     if(arguments.length === 0 || arguments.length === 1) {
         if((arguments.length === 1 && typeof arguments[0] === 'string' ) || (arguments.length === 1 && arguments[0].constructor === Array)) {
@@ -51,7 +53,7 @@ module.exports = function EasyQuery(scss) {
     });
     
     function generateRange(framework,key) {
-        return "-range: (" + framework[key] + "rem, calc(" + framework[key + 1] + "rem - 1px));\n";
+        return "-range: (" + framework[key] + "rem, " + (framework[key + 1] - $rpx) + "rem);\n";
     }
 
     function buildLibrary(framework) {
@@ -59,7 +61,7 @@ module.exports = function EasyQuery(scss) {
         switch(framework.length) {
             case 1: {
                 queued +=
-                    $sm + "-only: '#{$screen} and (max-width: calc(" + framework[0] + "rem - 1px))';\n" +
+                    $sm + "-only: '#{$screen} and (max-width: " + (framework[0] - $rpx) + "rem)';\n" +
                     $lg + "-only: '#{$screen} and (min-width: " + framework[0] + "rem)';";
                 break;
             }
@@ -67,7 +69,7 @@ module.exports = function EasyQuery(scss) {
                 queued +=
                     $md + generateRange(framework,0) +
 
-                    $sm + "-only: '#{$screen} and (max-width: calc(" + framework[0] + "rem - 1px))';\n" +
+                    $sm + "-only: '#{$screen} and (max-width: " + (framework[0] - $rpx) + "rem)';\n" +
 
                     $md + "-only: '#{$screen} and (min-width: " + framework[0] + "rem) and (max-width: #{upper-bound(" + $md + "-range)})';\n" +
                     $md + "-up: '#{$screen} and (min-width: " + framework[0] + "rem)';\n" +
@@ -81,7 +83,7 @@ module.exports = function EasyQuery(scss) {
                     $sm + generateRange(framework,0) +
                     $md + generateRange(framework,1) +
 
-                    $xs + "-only: '#{$screen} and (max-width: calc(" + framework[0] + "rem - 1px))';\n" +
+                    $xs + "-only: '#{$screen} and (max-width: " + (framework[0] - $rpx) + "rem)';\n" +
 
                     $sm + "-only: '#{$screen} and (min-width: " + framework[0] + "rem) and (max-width: #{upper-bound(" + $sm + "-range)})';\n" +
                     $sm + "-up: '#{$screen} and (min-width: " + framework[0] + "rem)';\n" +
@@ -100,7 +102,7 @@ module.exports = function EasyQuery(scss) {
                     $md + generateRange(framework,1) +
                     $lg + generateRange(framework,2) +
 
-                    $xs + "-only: '#{$screen} and (max-width: calc(" + framework[0] + "rem - 1px))';\n" +
+                    $xs + "-only: '#{$screen} and (max-width: " + (framework[0] - $rpx) + "rem)';\n" +
 
                     $sm + "-only: '#{$screen} and (min-width: " + framework[0] + "rem) and (max-width: #{upper-bound(" + $sm + "-range)})';\n" +
                     $sm + "-up: '#{$screen} and (min-width: " + framework[0] + "rem)';\n" +
@@ -125,7 +127,7 @@ module.exports = function EasyQuery(scss) {
                     $lg + generateRange(framework,2) +
                     $xl + generateRange(framework,3) +
 
-                    $xs + "-only: '#{$screen} and (max-width: calc(" + framework[0] + "rem - 1px)';\n" +
+                    $xs + "-only: '#{$screen} and (max-width: " + (framework[0] - $rpx) + "rem)';\n" +
 
                     $sm + "-only: '#{$screen} and (min-width: " + framework[0] + "rem) and (max-width: #{upper-bound(" + $sm + "-range)})';\n" +
                     $sm + "-up: '#{$screen} and (min-width: " + framework[3] + "rem)';\n" +
